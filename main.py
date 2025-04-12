ac126d6e5c4269f412173c03fce4a06a562f87a3
Vaibhav Adhe <vaibhavadhe0248@gmail.com>
2025-04-12 19:26:52 +0530

    hood design done

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import eel
+# import subprocess
+
 
 
 from engine.features import *
@@ -28,3 +30,4 @@ def start():
     os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
     eel.start('index.html', mode=None, host='localhost', block=True)
+    # import 
